31ab95749dfe0cb07885142565174aebd47649b4
Darkned07 <madaminovmuhammadyusuf806@gmail.com>
2024-08-20 23:47:25 +0500

    update telegram bot

--- bot.js
+++ bot.js
@@ -5,6 +5,8 @@ const userModel = require("./lib/mongoose/user.model");
 const { default: mongoose } = require("mongoose");
 
 const bot_token = process.env.BOT_TOKEN;
+const admin_username = process.env.ADMIN_USERNAME;
+const admin_id = process.env.ADMIN_ID;
 
 const bot = new TelegramBot(bot_token, {
   polling: true,
@@ -19,17 +21,22 @@ async function main() {
     if (mes.text === "/start") {
       const use_user = users.filter((u) => u.chatId === mes.from.id);
       if (mes.from.id === use_user[0].chatId) {
-        console.log("1", use_user);
-        bot.sendMessage(mes.from.id, "SIz avval botdan foydalangansiz!");
+        if (
+          mes.from.id === Number(admin_id) &&
+          mes.from.username === admin_username
+        ) {
+          admin_startBot(mes);
+        } else {
+          user_startBot(mes);
+        }
       } else {
-        console.log("2", use_user);
         messageBot(mes);
       }
     }
   });
 }
 
-async function messageBot(mes) {
+function messageBot(mes) {
   const chatId = mes.from.id;
   const name = mes.from.first_name;
   const user = mes.from.username;
@@ -42,7 +49,40 @@ async function messageBot(mes) {
 
   newUser.save();
 
-  bot.sendMessage(chatId, `Salom @${user} yoki ${name}`);
+  bot.sendMessage(chatId, `Salom @${user} biznig bo'timizga xush kelibsiz ✅`);
+}
+
+function user_startBot(u) {
+  console.log(u);
+  bot.sendMessage(
+    u.from.id,
+    `<b>Assalomu Alaykum ${u.from.first_name}</b>` +
+      `
+
+` +
+      "<i>Kinoni ko'rishni istasangiz kino kodini botga yuboring...</i>",
+
+    {
+      parse_mode: "HTML",
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: "Kino kodlari kanali...",
+              url: "https://t.me/web_kino",
+            },
+          ],
+        ],
+      },
+    }
+  );
+}
+
+function admin_startBot(u) {
+  bot.sendMessage(
+    u.from.id,
+    `Salom admin ${u.from.first_name} ishni boshlashingiz mumkun ✅`
+  );
 }
 
 main();
